156473710dc5a67c06b51e8871464d8f80dbcbe4
kirthich1008-bit <kirthi.ch1008@gmail.com>
2026-05-23 21:17:43 +0530

    completed

--- sharing-task.js
+++ sharing-task.js
@@ -37,6 +37,27 @@ reusable functions that solve specific tasks. This activity encourages:
 // 4. Return the result.
 
 
+// PSEUDOCODE: Task 1:
+// 1.create a function, name function as generateeAttendeeBadge and assign two parameter "name , role".
+// 2.in function body write
+//    1.captilize/uppercase the starting letter of role and rest should be in lowercase.
+//    2.write return statment stating what function has to output with its parameter.
+// 3.close function body.
+// 4.use console to enter String arguments for function parameters.
+
+
+
+function generateAttendeeBadge(name, role){
+let captalizedRole = role.charAt(0).toUpperCase() + role.slice(1).toLowerCase();
+return `Name: ${name} Role: ${captalizedRole}`;
+}
+console.log(generateAttendeeBadge("Alice", "Speaker"));
+console.log(generateAttendeeBadge("Brian", "volunteer"));
+console.log(generateAttendeeBadge("Rhea", "tressurer"));
+
+
+
+
 // ============================================
 // 🧩 Task 2: Calculate Event Cost
 // ============================================
@@ -52,7 +73,37 @@ reusable functions that solve specific tasks. This activity encourages:
 // 4. Return the final total.
 
 
+
+// PSEDUCODE: Task 2:
+// 1. create a function and name its function as calculate event cost and add two parameters attendees and cost per attendee.
+// 2.open function body:
+     // 1. multiply attendee and costperattendee to know the cost of event :
+     // 2. use while loop to check if attendees are more than 100.
+     // 3. open while body
+     // 4. if attendees are more than 100 give them 10% discount 
+     // 5. close while loop body
+     // 6. write return for total.
+     // 7. close the function body.
+// 8. write console for calculating event cost enter two parameters which are Number datatype.
+// 9. write two consoles which will have attendees more than 100 and other less than 100 to check weather the code is working correctly.
+
+
+
+
+function calculateEventCost(attendees, costperattendee){
+    let total = attendees * costperattendee;
+    if(attendees > 100){
+        total *= 0.90;
+    }
+    return total
+}
+console.log(calculateEventCost(120, 25));
+console.log(calculateEventCost(50, 15));
 // ============================================
+
+
+
+
 // 🧩 Task 3: Validate Email
 // ============================================
 // Create a function that:
@@ -63,6 +114,31 @@ reusable functions that solve specific tasks. This activity encourages:
 // Steps:
 // 1. Check if the string includes both "@" and ".".
 // 2. Return true or false accordingly.
+
+
+
+
+
+// PSEUDOCODE: Task 3:
+// 1. create function and name the function as isvalidate because we are using boolean function to validate email and paramter email.
+// 2. open function body:
+      // 1. return method we  use include method to check whether entered email has "@" and "." and 
+      //   use "&&" because bothe of them should be present to retun value as "true" or else it should return "false".
+// 3. close function body.
+// 4. write cosnsole funtion to check or validate email and also check by adding needed arguments are present or not.
+
+
+
+
+function isValidEmail(email){
+    return email.includes("@") && email.includes(".");
+}
+console.log(isValidEmail("riya@gmail.com"));
+console.log(isValidEmail("riyagmailcom"));
+console.log(isValidEmail("riya@@gmail.com"));
+console.log(isValidEmail("riya@gmail..com"));
+
+
 
 
 // ============================================
